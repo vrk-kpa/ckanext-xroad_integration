98e818d3b7763fe10bbee49a67d3d5237463c83b
Ian Tuomi <ian.tuomi@gofore.com>
2016-03-23 17:38:22 +0200

    LK-146: Sample code to access xroad-rest-gateway (untested)

--- ckanext/xroad_integration/harvesters/xroad_harvester.py
+++ ckanext/xroad_integration/harvesters/xroad_harvester.py
@@ -1,6 +1,13 @@
 from ckan.plugins.core import SingletonPlugin, implements
 from ckanext.harvest.interfaces import IHarvester
 from ckanext.harvest.harvesters import HarvesterBase
+from ckanext.harvest.model import HarvestJob, HarvestObject, HarvestGatherError
+
+import logging
+import json
+import requests
+
+log = logging.getLogger(__name__)
 
 class XRoadHarvesterPlugin(HarvesterBase):
 
@@ -17,14 +24,29 @@ class XRoadHarvesterPlugin(HarvesterBase):
         return ""
 
     def gather_stage(self, harvest_job):
+        log.debug('In xroad harvester gather_stage')
+
+        #members = self.get_xroad_catalog(harvest_job.source.url, harvest_job.since_date)
+        members = self.get_xroad_catalog("http://localhost:9090/rest-gateway-0.0.8-SNAPSHOT/Consumer/catalog", "2011-01-01")
+
+        for member in self._parse_xroad_data(members):
+            log.info(json.dumps(member))
 
         return []
 
-
     def fetch_stage(self, harvest_object):
-
+        log.debug('Doing nothing in xroad harvester fetch stage')
         return True
 
     def import_stage(self, harvest_object):
-
+        log.debug('In xroad havester import stage')
         return True
+
+    def _get_xroad_catalog(self, url, changed_after):
+        r = requests.get(url, parameters = {'changedAfter' : changed_after}, headers = {'Accept': 'application/json'})
+        if r.status_code != requests.codes.ok:
+            raise HarvestGatherError(msg = "Harvest failed!")
+        return r.json()
+
+    def _parse_xroad_data(self, res):
+        return res.json()['ListMembersResponse']['memberList']['members']
