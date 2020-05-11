337fc1db38c7e6b361041862da9370e26ed7210f
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-05-11 06:59:39 +0300

    LIKA-104: Fix selecting subsystem

--- ckanext/xroad_integration/helpers.py
+++ ckanext/xroad_integration/helpers.py
@@ -1,5 +1,5 @@
 
 def xroad_subsystem_path(dataset_dict):
-    field_names = ('xroad_instance', 'xroad_member_code', 'xroad_subsystem_code')
+    field_names = ('xroad_instance', 'xroad_memberclass', 'xroad_membercode', 'xroad_subsystemcode')
     fields = [dataset_dict.get(field) for field in field_names]
     return '.'.join(fields) if all(fields) else None
