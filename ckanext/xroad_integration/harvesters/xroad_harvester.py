2dda913be5c11a34bd9d3f5d5e709d2fd2702788
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-07-30 13:31:10 +0300

    fix syntax error

--- ckanext/xroad_integration/harvesters/xroad_harvester.py
+++ ckanext/xroad_integration/harvesters/xroad_harvester.py
@@ -890,7 +890,7 @@ class XRoadHarvesterPlugin(HarvesterBase):
                     org_data['webpage_address'] = data_dict.get('webpage_address', {})
 
                 if not org.get('webpage_description_modified_in_catalog', False):
-                    org_data['webpage_description'] = data_dict.get('webpage_description' {})
+                    org_data['webpage_description'] = data_dict.get('webpage_description', {})
 
                 patch_context = context.copy()
                 patch_context['allow_partial_update'] = True
