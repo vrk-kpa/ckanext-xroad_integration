4a0468a837ecba1617c1c90912268aba178e5bd2
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-10-02 11:03:28 +0300

    LIKA-203: Add sysadmin checks

--- ckanext/xroad_integration/views/xroad_errors.py
+++ ckanext/xroad_integration/views/xroad_errors.py
@@ -1,12 +1,23 @@
 from flask import Blueprint
 
-xroad_errors = Blueprint(u'xroad_error', __name__, url_prefix=u'/xroad_errors')
+import ckan.model as model
+import logging
+from ckan.plugins.toolkit import render, check_access, NotAuthorized, abort, _, g
+
+log = logging.getLogger(__name__)
+
+xroad_errors = Blueprint(u'xroad_error', __name__, url_prefix=u'/admin/xroad_errors')
 
 def index():
+    try:
+        context = dict(model=model, user=g.user, auth_user_obj=g.userobj)
+        check_access(u'sysadmin', context)
+    except NotAuthorized:
+        abort(403, _(u'Need to be system administrator to administer'))
 
     return render('admin/xroad_errors.html')
 
-xroad_errors.add_url_rule(u'/', view_func=index)
+xroad_errors.add_url_rule(u'/', view_func=index, strict_slashes=False)
 
 def get_blueprints():
     return [xroad_errors]
