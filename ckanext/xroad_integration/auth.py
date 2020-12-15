946551a8a004012c022e432909b86bd0eab40199
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-12-15 12:34:05 +0200

    LIKA-203: If organization is not provided, user must be sysadmin

--- ckanext/xroad_integration/auth.py
+++ ckanext/xroad_integration/auth.py
@@ -1,8 +1,12 @@
 from ckan.plugins.toolkit import check_access, NotAuthorized, _
 
 def xroad_error_list(context, data_dict):
-    try:
-        check_access('organization_update', context, {"id": data_dict.get('organization')})
-        return {"success": True}
-    except NotAuthorized:
-        return {"success": False, "msg": _(u"User not authorized to view X-Road error list for organization")}
+
+    if not data_dict.get('organization'):
+        check_access('sysadmin', context)
+    else:
+        try:
+            check_access('organization_update', context, {"id": data_dict.get('organization')})
+            return {"success": True}
+        except NotAuthorized:
+            return {"success": False, "msg": _(u"User not authorized to view X-Road error list for organization")}
