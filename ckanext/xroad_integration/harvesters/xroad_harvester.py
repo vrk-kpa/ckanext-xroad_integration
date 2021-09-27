ec0cd280e134fe80fe99fae75d4dba339304b8d2
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-09-27 12:07:50 +0300

    Add access_restriction_level to harvester defaults

--- ckanext/xroad_integration/harvesters/xroad_harvester.py
+++ ckanext/xroad_integration/harvesters/xroad_harvester.py
@@ -332,6 +332,7 @@ class XRoadHarvesterPlugin(HarvesterBase):
         package_dict['name'] = munge_title_to_name(dataset['subsystem']['subsystemCode'])
         package_dict['shared_resource'] = "no"
         package_dict['private'] = False
+        package_dict['access_restriction_level'] = "public"
 
         package_dict['xroad_instance'] = dataset['xRoadInstance']
         package_dict['xroad_memberclass'] = dataset['xRoadMemberClass']
@@ -403,6 +404,7 @@ class XRoadHarvesterPlugin(HarvesterBase):
                 "xroad_serviceversion": service_version,
                 "xroad_service_type": service_type,
                 "harvested_from_xroad": True,
+                "access_restriction_level": "public"
             }
 
             if service_description:
