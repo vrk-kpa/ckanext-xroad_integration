077f670d670e5606be0f1d7e29e0db0763e1697f
Jari-Pekka Voutilainen <jari-pekka.voutilainen@gofore.com>
2023-01-17 13:31:41 +0200

    Move plugins configuration to tests themselves

--- ckanext/xroad_integration/tests/test_plugin.py
+++ ckanext/xroad_integration/tests/test_plugin.py
@@ -77,9 +77,13 @@ def test_delete(xroad_rest_adapter_mocks):
 
 
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog scheming_datasets scheming_organizations fluent harvest '
+                                         'xroad_harvester xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('get_list_errors_data'))
-def test_xroad_errors(xroad_rest_adapter_mocks, xroad_rest_mocks):
+def test_xroad_errors(xroad_rest_adapter_mocks, xroad_rest_mocks, migrate_db_for):
+    migrate_db_for('xroad_integration')
+    
     harvester = XRoadHarvesterPlugin()
     run_harvest(url=xroad_rest_adapter_url('base'), harvester=harvester)
 
@@ -100,6 +104,7 @@ def test_xroad_errors(xroad_rest_adapter_mocks, xroad_rest_mocks):
 
 
 @pytest.mark.freeze_time('2023-01-17')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
 def test_list_xroad_errors_for_organization(migrate_db_for):
     migrate_db_for('xroad_integration')
@@ -163,7 +168,8 @@ def test_view_xroad_errors_for_organization(migrate_db_for, app):
 
 
 @pytest.mark.freeze_time('2022-01-02')
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address', xroad_rest_service_url('getListOfServices'))
 def test_fetch_xroad_service_list(xroad_rest_mocks, xroad_database_setup):
     result = call_action('fetch_xroad_service_list')
@@ -216,7 +222,8 @@ def test_fetch_xroad_service_list(xroad_rest_mocks, xroad_database_setup):
         ),
     ]
 )
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getListOfServices'))
 def test_fetch_xroad_service_list_with_date_ranges(xroad_rest_mocks,
@@ -226,7 +233,8 @@ def test_fetch_xroad_service_list_with_date_ranges(xroad_rest_mocks,
 
 
 @pytest.mark.freeze_time('2022-01-02')
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address', xroad_rest_service_url('getServiceStatistics'))
 def test_fetch_xroad_service_statistics(xroad_rest_mocks, xroad_database_setup):
     result = call_action('fetch_xroad_stats')
@@ -274,7 +282,8 @@ def test_fetch_xroad_service_statistics(xroad_rest_mocks, xroad_database_setup):
         ),
     ]
 )
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getServiceStatistics'))
 def test_fetch_xroad_service_statistics_with_date_ranges(xroad_rest_mocks,
@@ -284,7 +293,8 @@ def test_fetch_xroad_service_statistics_with_date_ranges(xroad_rest_mocks,
 
 
 @pytest.mark.freeze_time('2022-01-02')
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getDistinctServiceStatistics'))
 def test_fetch_xroad_distinct_service_statistics(xroad_rest_mocks, xroad_database_setup):
@@ -331,7 +341,8 @@ def test_fetch_xroad_distinct_service_statistics(xroad_rest_mocks, xroad_databas
         ),
     ]
 )
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getDistinctServiceStatistics'))
 def test_fetch_xroad_distinct_service_statistics_with_date_ranges(xroad_rest_mocks,
@@ -340,7 +351,8 @@ def test_fetch_xroad_distinct_service_statistics_with_date_ranges(xroad_rest_moc
     assert result['message'] == exp
 
 
-@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup', 'xroad_database_setup')
+@pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'xroad_database_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address', xroad_rest_service_url('heartbeat'))
 def test_xroad_heartbeat(xroad_rest_mocks):
     result = call_action('fetch_xroad_heartbeat')
@@ -349,6 +361,8 @@ def test_xroad_heartbeat(xroad_rest_mocks):
 
 
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup', 'xroad_database_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog scheming_datasets scheming_organizations fluent harvest '
+                                         'xroad_harvester xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getOrganizationOrganizationData'))
 def test_xroad_get_organizations_organization_data(xroad_rest_mocks):
@@ -375,6 +389,8 @@ def test_xroad_get_organizations_organization_data(xroad_rest_mocks):
 
 
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup', 'xroad_database_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog scheming_datasets scheming_organizations fluent harvest '
+                                         'xroad_harvester xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getOrganizationCompanyData'))
 def test_xroad_get_organizations_company_data(xroad_rest_adapter_mocks, xroad_rest_mocks):
@@ -398,6 +414,8 @@ def test_xroad_get_organizations_company_data(xroad_rest_adapter_mocks, xroad_re
 
 
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup', 'xroad_database_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog scheming_datasets scheming_organizations fluent harvest '
+                                         'xroad_harvester xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address',
                          xroad_rest_service_url('getOrganizationCompanyDataWithBusinessIdChanges'))
 def test_xroad_get_organizations_company_data_with_business_id_changes(xroad_rest_adapter_mocks, xroad_rest_mocks):
@@ -414,6 +432,8 @@ def test_xroad_get_organizations_company_data_with_business_id_changes(xroad_res
 
 
 @pytest.mark.usefixtures('with_plugins', 'clean_db', 'clean_index', 'harvest_setup', 'xroad_database_setup')
+@pytest.mark.ckan_config('ckan.plugins', 'apicatalog scheming_datasets scheming_organizations fluent harvest '
+                                         'xroad_harvester xroad_integration')
 @pytest.mark.ckan_config('ckanext.xroad_integration.xroad_catalog_address', xroad_rest_service_url('getOrganizationEmptyData'))
 def test_xroad_get_organizations_empty_data(xroad_rest_adapter_mocks, xroad_rest_mocks):
     harvester = XRoadHarvesterPlugin()
